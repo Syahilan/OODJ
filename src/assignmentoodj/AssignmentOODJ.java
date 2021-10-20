b485102419b620e992ed3208fc8fbdce12f72a2d
Syahilan <tp062031@mail.apu.edu.my>
2021-10-20 18:51:12 +0800

    This is the println commit!

--- src/assignmentoodj/AssignmentOODJ.java
+++ src/assignmentoodj/AssignmentOODJ.java
@@ -11,7 +11,7 @@ public class AssignmentOODJ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Welcome to the Covid-19 Vaccine registration system!");
     }
     
 }
