6e822c7578e6f8ccecefe6978e1576062e94b4ae
Darshini <Darshini@LAPTOP-J3HV4UAH>
2021-11-28 17:25:23 +0400

    Vaccine name enum

---
+++ src/assignmentoodj/VaccineName.java
@@ -0,0 +1,8 @@
+/*
+ * This is proprietary code written by Syahilan A/L Selvarajoo [TP062031] & Darshini Dakshyani Beerachee [TP060519]
+ * APD2F2109CS(DA) || CT038-3-2-OODJ-LAB-18
+ */
+package assignmentoodj;
+public enum VaccineName {
+    Moderna, Pfizer, CanSino, Janssen, AstraZeneca, Sinopharm, Sinovac, Sputnik;
+}
