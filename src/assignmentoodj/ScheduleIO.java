1f154dac196338b404c813c0c3d9d2f869467810
Darshini <Darshini@LAPTOP-J3HV4UAH>
2021-12-03 14:34:03 +0400

    Schedule Read and Write files

---
+++ src/assignmentoodj/ScheduleIO.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignmentoodj;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Darshini
+ */
+public class ScheduleIO {
+        public static ArrayList<CentreSchedule> allSchedules
+            = new ArrayList<CentreSchedule>();
+        public static void read(){
+        try{
+            Scanner readSchFile = new Scanner(new File("Schedule.txt"));
+            while (readSchFile.hasNext()) {
+                int sId = Integer.parseInt(readSchFile.nextLine());
+                String centre = readSchFile.nextLine();
+                String date = readSchFile.nextLine();
+                String sTime = readSchFile.nextLine();
+                String eTime = readSchFile.nextLine();
+                String name = readSchFile.nextLine();
+                int quantity = Integer.parseInt(readSchFile.nextLine());
+                readSchFile.nextLine();
+                CentreSchedule s = new CentreSchedule(sId, centre, date, sTime, eTime,  name, quantity);
+                allSchedules.add(s);
+            }
+            
+        } catch(FileNotFoundException e){
+            System.out.println("File not found !");
+            try{
+            PrintWriter writeSchFile = new PrintWriter("Schedule.txt");
+            } catch(Exception ex){
+            System.out.println("Error in write!");
+        }
+            
+        } catch(Exception e){
+            System.out.println("Error in read!");
+          }
+        
+    }
+    public static int write(){
+        try{         
+            PrintWriter writeSchFile = new PrintWriter("Schedule.txt");
+            for (int i = 0; i < allSchedules.size(); i++) {
+                writeSchFile.println(allSchedules.get(i).getsId());
+                writeSchFile.println(allSchedules.get(i).getCentre());
+                writeSchFile.println(allSchedules.get(i).getDate());
+                writeSchFile.println(allSchedules.get(i).getStartTime());
+                writeSchFile.println(allSchedules.get(i).getEndTime());
+                writeSchFile.println(allSchedules.get(i).getVacName());
+                writeSchFile.println(allSchedules.get(i).getQuantity());
+                writeSchFile.println();
+            }
+            writeSchFile.close();
+        } catch(Exception e){
+            System.out.println("Error in write!");
+            return -1;
+        }
+        return 0;
+    }
+}
